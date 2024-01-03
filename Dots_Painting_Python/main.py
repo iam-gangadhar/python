965baf17eee0b5c2443b5cb7a1d1dd6323d6f723
gangadharDell <ganagdharDell@gmail.com>
2024-01-03 13:57:38 +0530

    updating the today learning

---
+++ Dots_Painting_Python/main.py
@@ -0,0 +1,35 @@
+import turtle
+from turtle import Turtle,Screen
+
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("Red")
+x = 0
+# Drawing Square with Turtle
+# for i in range(4):
+#     tim.setheading(x)
+#     tim.forward(100)
+#     x = x+90
+#
+# # Draw Dashed Line.
+# tom = Turtle()
+
+# Drawing Dashed Lines Using Turtle
+# for i in range(50):
+#     tom.pendown()
+#     tom.forward(10)
+#     tom.penup()
+#     tom.forward(10)
+
+# Drawing Different Shapes using Turtle
+
+
+
+screen = Screen()
+
+
+
+
+
+screen.exitonclick()
