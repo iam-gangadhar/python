16b7073f6a071529f3418bbf99a16be5d012f0a8
gangadharDell <ganagdharDell@gmail.com>
2024-01-05 15:07:44 +0530

    updating the today learning

--- xxxx.py
+++ xxxx.py
@@ -2,3 +2,25 @@
 
 # Updating some text using pycharm
 print("hello world")
+
+# Inheritence Demo In Python
+
+class Animal:
+    def __init__(self):
+        self.Eyes = 2
+    
+    def breath(self):
+        print("Inhaling and Exhaling")
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+        self.food = "water"
+    
+    def swim(self):
+        print("I am swimming inside water")
+
+nano = Fish()
+print(nano.food)
+nano.swim()
+nano.breath()
