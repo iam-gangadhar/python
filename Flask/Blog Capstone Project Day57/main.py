b9bd6f64e1adbc3eae27cb9b24811496b2cba1e5
gangadharDell <ganagdharDell@gmail.com>
2024-01-29 17:51:36 +0530

    updatea-all

--- Flask/Blog Capstone Project Day57/main.py
+++ Flask/Blog Capstone Project Day57/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
@@ -6,6 +7,18 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route("/blog")
+def get_blog():
+    my_data = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
+    return render_template('index.html', blog_data=my_data.json())
+
+@app.route("/blog/post/<num>")
+def get_post(num):
+    print(num)
+    my_data = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
+    return render_template('post.html', page_number=num, blog_data=my_data.json())
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import requests
 
 
-name = "gangadhar" #input("Enter your name ?")
-response = requests.get(url=f"https://api.agify.io?name={name}")
-gender = requests.get(url=f"https://api.genderize.io?name={name}")
+# name = "gangadhar" #input("Enter your name ?")
+# response = requests.get(url=f"https://api.agify.io?name={name}")
+# gender = requests.get(url=f"https://api.genderize.io?name={name}")
+#
+# name_dictionary = response.json()
+# gender_dictionary = gender.json()
+#
+# print(name_dictionary["age"])
 
-name_dictionary = response.json()
-gender_dictionary = gender.json()
-
-print(name_dictionary["age"])
-
+my_data = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
+data = my_data.json()
+print(data)
